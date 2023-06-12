e63f9cd8ada43ef5d786794fcf0544965cace9df
sechi <sechichen@gmail.com>
2023-06-12 11:54:55 +0800

    chore: change file name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { SimulateLoginPlugin } from './components/TestCom'
+import { SimulateLoginPlugin } from './components/Container'
 import { LoginArea } from './components/LoginArea'
 
 export { SimulateLoginPlugin, LoginArea }
