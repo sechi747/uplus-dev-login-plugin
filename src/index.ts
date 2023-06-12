ca3e69204adc144dea22b17ba314dd3a2b5e1706
sechi <sechichen@gmail.com>
2023-06-12 11:55:28 +0800

    chore: remove useless export def

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import { SimulateLoginPlugin } from './components/Container'
-import { LoginArea } from './components/LoginArea'
 
-export { SimulateLoginPlugin, LoginArea }
+export { SimulateLoginPlugin }
