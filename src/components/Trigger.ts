4d246791d74ce024c440a11bcfd7213ae1b10f99
sechi <sechichen@gmail.com>
2023-06-12 18:02:20 +0800

    fix: h function param

--- src/components/Trigger.ts
+++ src/components/Trigger.ts
@@ -32,7 +32,7 @@ export const Trigger: Object = defineComponent({
         h(
           'div',
           transformVNodeProps({ class: 'dev-login-trigger', onClick: this.showDialog }),
-          h('div', { class: 'dev-login-profile-icon' }),
+          h('div', { class: 'dev-login-profile-icon' }, ''),
         ),
         h(
           Dialog,
