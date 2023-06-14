f25a7a2da09f472f15b8580b5183bd4c0a8d3996
sechi <sechichen@gmail.com>
2023-06-14 15:12:42 +0800

    feat: show error message when login

--- src/components/LoginArea.ts
+++ src/components/LoginArea.ts
@@ -5,6 +5,7 @@ export const LoginArea: Object = defineComponent({
   setup(_, { emit }) {
     const account = ref('')
     const password = ref('')
+    const errorMessage = ref('')
 
     const handleAccountInput = (e: InputEvent) => {
       account.value = (e.target as HTMLInputElement).value
@@ -30,13 +31,19 @@ export const LoginArea: Object = defineComponent({
           })
             .then(res => res.json())
             .then((result) => {
-              const tokens = {
-                token: result.data.accessToken,
-                refreshToken: result.data.refreshToken,
-                xAccessToken: result.data['X-Access-Token'],
+              if (!result.success && result.message) {
+                errorMessage.value = result.message
               }
-              setAuthToken(tokens)
-              emit('afterLogin')
+              else {
+                const tokens = {
+                  token: result.data.accessToken,
+                  refreshToken: result.data.refreshToken,
+                  xAccessToken: result.data['X-Access-Token'],
+                }
+                setAuthToken(tokens)
+                emit('afterLogin')
+                errorMessage.value = ''
+              }
             })
         })
     }
@@ -47,6 +54,7 @@ export const LoginArea: Object = defineComponent({
       handleLogin,
       account,
       password,
+      errorMessage,
     }
   },
   render() {
@@ -57,6 +65,7 @@ export const LoginArea: Object = defineComponent({
         h('input', transformVNodeProps({ onInput: this.handleAccountInput, placeholder: '请输入账号', class: 'dev-login-input' })),
         h('input', transformVNodeProps({ onInput: this.handlePasswordInput, type: 'password', placeholder: '请输入密码', class: 'dev-login-input' })),
         h('button', transformVNodeProps({ onClick: this.handleLogin, class: 'dev-login-button' }), '登录'),
+        this.errorMessage ? h('span', transformVNodeProps({ style: { color: '#d32f2f' } }), this.errorMessage) : '',
       ])
   },
 })
